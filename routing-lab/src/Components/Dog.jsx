06b00fabe23b6a0b7dbf4383ca469b7c7dcca820
tbriany <brianytaveras@pursuit.org>
2019-12-22 19:28:25 -0500

    able to get random dog picture from Dog component

--- routing-lab/src/Components/Dog.jsx
+++ routing-lab/src/Components/Dog.jsx
@@ -11,16 +11,26 @@ class Dog extends React.Component {
 
     componentDidMount() {
         console.log('component mounted')
+        this.handleRandomDog()
     }
 
     handleRandomDog = async () => {
-        
+        let url = "https://dog.ceo/api/breeds/image/random"
+        let {data} = await axios.get(url)
+        this.setState({
+            url: data.message
+        })
     }
 
+
     render(){
+        const {url} = this.state
+        const {handleRandomDog} = this
         return (
             <div>
             <h1>Dogs</h1>
+            <img src={url}  alt="dog pic"></img><br></br>
+            <button onClick={handleRandomDog}>New Dog</button>
             </div>
         )
     }
