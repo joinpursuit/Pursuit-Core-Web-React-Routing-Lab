64783661ae29d75097dde56bba412b0728f82f3c
Henrynunez112 <henrynunez@pursuit.org>
2020-02-26 20:56:29 -0500

    created Dogs component

---
+++ catdog/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dogs from './Component/Dogs'
+
+function App() {
+  return (
+    <div className="App">
+      <Dogs />
+    </div>
+  );
+}
+
+export default App;
