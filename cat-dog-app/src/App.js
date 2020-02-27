1c46c059b03b18c495c21e1b937c25c43200ce7c
bsihame <sihamebazi@pursuit.org>
2020-02-26 21:18:44 -0500

    add routees in app js file

--- cat-dog-app/src/App.js
+++ cat-dog-app/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import NavBar from './components/NavBar';
+import Dog from './components/Dog';
+import Cat from './components/Cat';
+import RandomDogByBreed from './components/MultipleRandomDogs';
+import MultipleRandomDogs from './components/MultipleRandomDogs';
+import MultipleRandomCat from './components/MultipleRandomCats';
+import { Route, Switch } from 'react-router-dom'
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Route path={'/dog/random'} component={Dog}/>
+      <Route path={'/cat/random'} component={Cat}/>
+      <Route path={'/dog/random/:num'} component={MultipleRandomDogs}/>
+      <Route path={'/dog/:breed'} component={RandomDogByBreed}/>
+      <Route path={'/cat/random/:num'} component={MultipleRandomCat}/>
+
       
     </div>
   );
