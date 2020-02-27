6925f0f1b98af6fd7b76cd32c76d2952cbd10d64
kongnyc <bbsyoow@gmail.com>
2020-02-26 21:27:56 -0500

    finish random dog

--- routingcatdog/src/Components/DogImageRandom.js
+++ routingcatdog/src/Components/DogImageRandom.js
@@ -1,8 +1,41 @@
-import React from 'react'
-import axios from 'axios'
 
-const DogImageRandom = async() => {
 
+import React, { Component } from 'react'
+import axios from "axios"
+
+
+export class DogImageRandom extends Component {
+    state={
+        imgLink:null
+    }
+
+    async handleRandomDogBtn(){
+        // debugger
+        let res = await axios.get("https://dog.ceo/api/breeds/image/random")
+        try {
+            this.setState(
+                {imgLink:res.data.message}
+                )
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    componentDidMount(){
+        this.handleRandomDogBtn()
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Random Dog is here</h1>
+                <br></br>
+                <button onClick={this.handleRandomDogBtn.bind(this)}>new random dog</button>
+                <br></br>
+                <img src={this.state.imgLink} alt="dog"></img>
+            </div>
+        )
+    }
 }
 
-export default DogImageRandom;
+export default DogImageRandom
