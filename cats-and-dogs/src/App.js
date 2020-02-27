3340ff70e7bf810dd4bda5899c0507d62e330894
Kevin Brutus <kevinbrutus@pursuit.org>
2020-02-26 20:47:25 -0500

    install react

---
+++ cats-and-dogs/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        Yerrr
+      </div>
+    );
+  }
+}
+
+export default App;
