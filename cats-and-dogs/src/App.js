81e1e86514643fadb8839c1342da29820d92e593
maria liza <mariamartinez@pursuit.org>
2020-02-29 12:49:26 -0500

    complete populateSelect

--- cats-and-dogs/src/App.js
+++ cats-and-dogs/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Random from "./components/Dogs/RandomImg.js"
 import DisplayDog from "./components/Dogs/DisplayDog"
+import GetDogByBreed from "./components/Dogs/dogByBreed"
 
 function App() {
   return (
     <div className="App">
       <DisplayDog/>
       <Random/>
+      <GetDogByBreed />
     </div>
   );
 }
