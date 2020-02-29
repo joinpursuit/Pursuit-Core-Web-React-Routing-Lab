53d73a4c6379151efc98a63493d2feb3997d9d40
Kevin Brutus <kevinbrutus@pursuit.org>
2020-02-29 10:16:53 -0500

    work on randomdogbybreed

--- cats-and-dogs/src/components/RandomDogByBreed.js
+++ cats-and-dogs/src/components/RandomDogByBreed.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from 'react-router-dom';
 
-const RandomDogByBreed = () => {
-    return(
-        <div>This my div</div>
-    )
+let url = ""
+
+// const getDogPics = async () => {
+// debugger
+//   let  {breed} = useParams();
+//   const breedURL = `https://dog.ceo/api/breed/${breed}/images/random/1`;
+//   try {
+//     let res = await axios.get(breedURL);
+//     url = res.data.message;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+debugger
+
+const RandomDogByBreed =  () => {
+let  {breed} = useParams();
+    
+  const breedURL = `https://dog.ceo/api/breed/${breed}/images/random/1`;
+  try {
+    let res =  axios.get(breedURL);
+    url = res.data.message;
+  } catch (error) {
+    console.log(error);
+  }
+//   componentDidMount() {
+//     this.getDogPics(this.state.numberOfDogs)
+//   }
+
+//   render() {
+    // let dogs = this.state.dogPics.map(dog => {
+    //   return <DogDisplay src={dog} key={dog} />;
+    // });
+    
+    return <img src= {url} alt={""}></img>;
+//   }
 }
-
 export default RandomDogByBreed;
