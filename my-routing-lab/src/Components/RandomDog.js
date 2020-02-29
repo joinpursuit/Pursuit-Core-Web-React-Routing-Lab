d13bb8b92b9b925262b52742d005ac76b7e3f5d0
Nilber Remon <nilberremon@gmail.com>
2020-02-29 12:16:29 -0500

    Build Random Cat and Random Dog Apps

--- my-routing-lab/src/Components/RandomDog.js
+++ my-routing-lab/src/Components/RandomDog.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import Image from './Image'
 import axios from 'axios';
 
 class RandomDog extends React.Component {
     state = {
         image: ""
     }
-    componentDidMount() {
-
-    }
+    // componentDidMount() { //Render Image on Mount
+    //     this.getImage()
+    // }
     getImage = async () => {
-        let res = await axios.get("")
+        try {
+            let res = await axios.get("https://dog.ceo/api/breeds/image/random")
+            this.setState({image: res.data.message})
+        } catch (error) {
+                this.setState({image: ""})
+                console.log(`Recognize the error of your ways: ${error}`)
+        }
     }
-
     render () {
+        const {image} = this.state
         return (
-            "dog"
+            <div>
+                <button onClick={this.getImage}>Get Random Dog</button>
+                <Image image={image}/>
+            </div>
         )
     }
-
-
-
-
 }
 
 export default RandomDog; 
