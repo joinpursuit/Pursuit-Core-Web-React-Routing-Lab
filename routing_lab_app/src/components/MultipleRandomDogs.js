2d4ac7a450b956386f4b93fd2f0c83f881a30fa4
Kevin Wong <kevinwong@pursuit.org>
2020-03-01 12:15:56 -0500

    refactor lab to use hooks for RandomDog and MultipleRDogs

--- routing_lab_app/src/components/MultipleRandomDogs.js
+++ routing_lab_app/src/components/MultipleRandomDogs.js
@@ -1,37 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dog from "./Dogs";
 import { useParams } from "react-router-dom";
 
-class MultipleRandomDogs extends Component {
-  state = {
-    manyDogs: [],
-    numberOfDogs: 1
-  };
+const MultipleRandomDogs = () => {
+  const [dogPics, setDogs] = useState([]);
+  const { num } = useParams();
 
-  componentDidMount() {
-    const { numberOfDogs } = this.state;
-    if (numberOfDogs) {
-      this.getManyRandomDogs(numberOfDogs);
-    }
-  }
+  useEffect(() => {
+    const getManyRandomDogs = async num => {
+      try {
+        const dogURL = `https://dog.ceo/api/breeds/image/random/${num}`;
+        let res = await axios.get(dogURL);
+        const dogs = res.data.message.map(dog => {
+          return <Dog img={dog} />;
+        });
+        setDogs(dogs);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getManyRandomDogs(num);
+  }, [num]);
 
-  getManyRandomDogs = async numberOfDogs => {
-    try {
-      const dogURL = `https://dog.ceo/api/breeds/image/random/${numberOfDogs}`;
-      let res = await axios.get(dogURL);
-      this.setState({ manyDogs: res.data.message });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  render() {
-    let manyDogPics = this.state.manyDogs.map(dog => {
-      return <Dog img={dog} key={dog} />;
-    });
-    return <div>{manyDogPics}</div>;
-  }
-}
+  return <div>{dogPics}</div>;
+};
 
 export default MultipleRandomDogs;
--- routing_lab_app/src/components/RandomDog.js
+++ routing_lab_app/src/components/RandomDog.js
@@ -1,35 +1,29 @@
-import React, {Component} from "react";
-import Dog from "./Dogs"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import Dog from "./Dogs";
+import axios from "axios";
 
-class RandomDog extends Component{
-    state={
-        dogPic: ""
+const RandomDog = () => {
+  const [dogPic, setDogPic] = useState("");
+
+  const getRandomDog = async () => {
+    try {
+      let res = await axios.get("https://dog.ceo/api/breeds/image/random");
+      setDogPic(res.data.message);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    getRandomDog = async()=>{
-        try{
-            let res = await axios.get("https://dog.ceo/api/breeds/image/random")
-            debugger
-            this.setState({dogPic:res.data.message})
-        }catch(err){
-            console.log(err)
-        }
-    }
+  useEffect(() => {
+    getRandomDog();
+  }, []);
 
-
-
-
-    render(){
-       
-        return(
-            <div>
-                <button onClick={this.getRandomDog}>Get Random Dog</button>
-                <Dog img={this.state.dogPic}/>
-            </div>
-        )
-    }
-}
-
+  return (
+    <div>
+      <button onClick={getRandomDog}>Get Random Dog</button>
+      <Dog img={dogPic} />
+    </div>
+  );
+};
 
 export default RandomDog;
