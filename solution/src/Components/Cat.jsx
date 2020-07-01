fc143b3d37683c3ad6162b0540a4cfa37c52294f
gsanchez13 <gisellesanchez@pursuit.org>
2020-06-30 23:45:26 -0400

    changes component from class to functional

--- solution/src/Components/Cat.jsx
+++ solution/src/Components/Cat.jsx
@@ -1,43 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-class Cat extends Component {
-    constructor(props) {
-        super();
-        this.state = {
-            url: "",
-        }
-    }
-    componentDidMount = () => {
-        this.getRandomCat();
-    }
-    getRandomCat = async () => {
-        const { number } = this.state;
-        if(!number){
-        let url = `https://api.thecatapi.com/v1/images/search`;
+const Cat = () => {
+    const { num } = useParams();
+    const [catArray, setCatArray] = useState([]);
+    const [numberOfCats, setNumberOfCats] = useState(1);
+    const [allBreeds, setAllBreeds] = useState([]);
+    useEffect(() => {
+        handleRandomCat();
+        generateCatCards();
+        // getBreeds();
+    }, []);
+
+    const handleRandomCat = async () => {
+        let url = ``;
+        num === '1' || num === 'undefined' ? url = `https://api.thecatapi.com/v1/images/search` : url = `https://api.thecatapi.com/v1/images/search?limit=${num}`;
         try {
-            let catObj = await axios.get(url).then((res) => { return res.data })
-            let catUrl = catObj.map((el) => { return el.url })
-            this.setState({
-                url: catUrl
-            })
+            let imageUrls = await axios.get(url)
+                .then((res) => { return res.data[0].url });
+            setCatArray([imageUrls])
         }
         catch (err) {
-            console.log("no gatos")
-        }
+            let errorMessage = `Uh-oh! Something went wrong, try again later!`;
+            console.log(errorMessage)
         }
     }
-    render() {
-        const { url } = this.state
-        return (
-            <div>
-                <h1 className='text-muted'> Gatos </h1>
-                <div className="images">
-                    <img src={url} alt={url}/>
+    const generateCatCards = () => {
+        return catArray.map((cat) => {
+            return (
+                <div key={cat} className=''>
+                    <img src={cat} alt='cat' className='w-25 h-25 mt-3' />
                 </div>
+            )
+        })
+    };
+    // const getBreeds = async () => {
+    //     let allBreedsArr = [];
+    //     let url = `https://api.thecatapi.com/v1/images/search`;
+    //     try {
+    //         let allBreedsObj = await axios.get(url).then((res) => { return res.data.message });
+    //         for (let breed in allBreedsObj) {
+    //             allBreedsArr.push(breed)
+    //         }
+    //         setAllBreeds(allBreedsArr)
+    //     }
+    //     catch (err) {
+    //         console.log(err)
+    //     }
+    // };
+    // const breedList = () => {
+    //     return allBreeds.map((breed) => {
+    //         return (
+    //             <li className="dropdown-item" key={breed}>{breed}</li>
+    //         )
+    //     })
+    // };
+    return (
+        <div>
+            <h1 className='text-muted mt-3'> Gatos </h1>
+            <div className="btn-group dropright">
+                <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Select Breed
+                </button>
+                <ul className="dropdown-menu scrollable-menu" role='menu'>
+                    {/* {breedList()} */}
+                    <div className="dropdown-divider"></div>
+                    <li className="dropdown-item">random</li>
+                </ul>
             </div>
-
-        )
-    }
+            <div className="carousel-inner mt-5">
+                {generateCatCards()}
+            </div>
+        </div>
+    )
 }
 export default Cat;
